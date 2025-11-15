0882aec76abc838634c214673d95205416b84994
Suzaa <suzaaglobal@gmail.com>
2025-11-15 22:57:45 +0200

    Refactor _meta.js to use default export

--- pages/_meta.js
+++ pages/_meta.js
@@ -1,10 +1,10 @@
-{
-  "index": "Home",
+export default {
+  index: "Home",
   "01-introduction": "Introduction",
   "02-product": "Product",
   "03-developers": "Developers",
   "04-operations": "Operations",
   "05-legal": "Legal",
   "06-support": "Support",
-  "advanced": "Advanced"
+  advanced: "Advanced"
 }
