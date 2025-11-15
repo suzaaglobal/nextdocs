aa04c1cae32b77bde3f293e4d148e6609b01e575
Suzaa <suzaaglobal@gmail.com>
2025-11-15 22:53:36 +0200

    Update _meta.js

--- pages/_meta.js
+++ pages/_meta.js
@@ -1,8 +1,10 @@
-export default {
-  index: 'Home',
-  '01-introduction': 'Introduction',
-  '02-product': 'Product',
-  '03-developers': 'Developers',
-  '04-operations': 'Operations',
-  '05-legal': 'Legal'
+{
+  "index": "Home",
+  "01-introduction": "Introduction",
+  "02-product": "Product",
+  "03-developers": "Developers",
+  "04-operations": "Operations",
+  "05-legal": "Legal",
+  "06-support": "Support",
+  "advanced": "Advanced"
 }
