a519b76359389b40c56f9357740001b2c38e49a7
Suzaa Developer <dev@suzaa.global>
2025-11-15 16:45:25 +0000

    Fixed theme config and added docs

--- theme.config.jsx
+++ theme.config.jsx
@@ -1,6 +1,12 @@
-export default {
-  logo: <span>My Docs</span>,
+const config = {
+  logo: <span>Suzaa Docs</span>,
   project: {
     link: 'https://github.com/suzaaglobal/nextdocs'
+  },
+  docsRepositoryBase: 'https://github.com/suzaaglobal/nextdocs',
+  footer: {
+    text: 'Suzaa Documentation'
   }
 }
+
+export default config
