a4e1b2713a907cb712ec16dd464c492f8ab87c1b
Suzaa Developer <dev@suzaa.global>
2025-11-15 16:42:38 +0000

    Initial Nextra setup

---
+++ theme.config.tsx
@@ -0,0 +1,14 @@
+import { useConfig, type DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>Suzaa Docs</span>,
+  project: {
+    link: 'https://github.com/suzaaglobal/nextdocs',
+  },
+  docsRepositoryBase: 'https://github.com/suzaaglobal/nextdocs',
+  footer: {
+    text: 'Suzaa Documentation',
+  },
+}
+
+export default config
