e2b48ef2460003e3ffdeb10406b2fdcccc6cb9fb
Suzaa <suzaaglobal@gmail.com>
2025-11-16 22:11:57 +0200

    Add '04-architecture' entry to _meta.js

--- pages/01-introduction/_meta.js
+++ pages/01-introduction/_meta.js
@@ -2,5 +2,6 @@ export default {
   index: 'Overview',
   '01-getting-started': 'Getting Started',
   '02-core-concepts': 'Core Concepts',
-  '03-architecture': 'Architecture'
+  '03-architecture': 'Architecture',
+  '04-architecture': 'Brand'
 }
